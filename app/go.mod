31cf995d60a93c46f3c80fe1922d216436c48578
Jett65 <jettc65@gmail.com>
2023-08-02 22:39:43 -0600

    seting up

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/Jett65/soundQueue/app
+
+go 1.20
---
+++ app/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/Jett65/soundQueue/sound"
+)
+
+func main() {
+    fmt.Println("Hello world")
+}
